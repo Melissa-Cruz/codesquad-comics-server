7578db6caa1e840c8c0eceb2abb3b0d700a1cc61
Melissa Cruz <melissa.e.cruz@gmail.com>
2025-05-16 20:53:38 -0400

    added the deleteBook, updateBook and createBook controller functions

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const booksData = require("../data/books");
-
-const getAllBooks = (req, res, next) => {
+// idk why I'm putting async but it's in slide and demo
+const getAllBooks = async (req, res, next) => {
   // response.send("This will send all of the book data")
 
   try {
@@ -20,7 +20,7 @@ const getAllBooks = (req, res, next) => {
   }
 };
 
-const getBook = (req, res, next) => {
+const getBook = async (req, res, next) => {
   const { _id } = req.params;
 
   try {
@@ -28,39 +28,97 @@ const getBook = (req, res, next) => {
 
     return res.status(200).json({
       success: { message: "This will send a single book by its id" },
-      data: {book:book},
+      data: { book: book },
       statusCode: 200,
     });
   } catch (error) {
     return res.status(400).json({
-        success: { message: "Resource not found. Search again" },
-        statusCode: 400,
-      });
+      error: { message: "Resource not found. Search again" },
+      statusCode: 400,
+    });
   }
 };
 
-const createBook = (request, response, next) => {
-  // response.send("This will create a new book")
-  response.status(200).json({
-    success: { message: "This will create a new book" },
-    statusCode: 200,
-  });
+const createBook = async (req, res, next) => {
+  const { title, author, publisher, genre, pages, rating, synopsis, imageUrl } =
+    req.body;
+
+  try {
+    const newBook = {
+      title,
+      author,
+      publisher,
+      genre,
+      pages,
+      rating,
+      synopsis,
+      imageUrl,
+    };
+
+    return res.status(201).json({
+      success: { message: "This will create a new book" },
+      data: { book: newBook },
+      statusCode: 201,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      error: { message: "Error failed to create a new book." },
+      statusCode: 400,
+    });
+  }
 };
 
-const updateBook = (request, response, next) => {
-  // response.send("This will update a book by its id")
-  response.status(200).json({
-    success: { message: "This will update a book by its id" },
-    statusCode: 200,
-  });
+const updateBook = async (req, res, next) => {
+    const { title, author, publisher, genre, pages, rating, synopsis, imageUrl } =
+    req.body;
+
+    const { _id } = req.params;
+
+    try{
+        const updatedBook = { title, 
+            author, 
+            publisher,
+            genre, 
+            pages, 
+            rating, 
+            synopsis, 
+            imageUrl 
+        }
+        return res.status(201).json({
+            success:{message:"The book was updated"}, 
+            data:{book:updatedBook},
+
+        });
+    }catch(error){
+        return res.status(400).json({
+            error:{message:"There was an error updating the book."},
+            statusCode: 400
+        });
+
+    }
+
 };
 
-const deleteBook = (request, response, next) => {
-  // response.send("This will delete a book by its id")
-  response.status(200).json({
-    success: { message: "This will delete a book by its id" },
-    statusCode: 200,
-  });
+const deleteBook = async (req, res, next) => {
+
+    const { _id } = req.params;
+
+    try{
+        const books = booksData.filter((book) = book._id !== _id);
+        
+        return res.status(200).json({
+            success:{message:"This will delete a book by it's id."},
+            data:{book: books},
+            statusCode: 200,
+        });
+
+    }catch(error){
+        return res.status(400).json({
+            error:{message:"There was an error while deleting the book."}, 
+            statusCode:400,
+        });
+
+    }
 };
 
 module.exports = { getAllBooks, getBook, createBook, updateBook, deleteBook };
