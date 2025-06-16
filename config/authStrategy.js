f65381b7b07718ef5590a24b2895a36408e9d4a9
Melissa Cruz <melissa.e.cruz@gmail.com>
2025-06-15 20:41:34 -0400

    updated the Google Strategy

--- config/authStrategy.js
+++ config/authStrategy.js
@@ -31,9 +31,15 @@ passport.use(
 
 
 passport.use(
-    new GoogleStrategy({clientID, clientSecret, callbackURL}, 
+    new GoogleStrategy({
+        clientID:process.env.GOOGLE_CLIENT_ID, 
+        clientSecret:process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL:"http://localhost:3000/auth/google/callback"
+    }, 
         async (accessToken,refreshToken, profile, done )=>{
             try{
+                const user = await User.findOneAndDelete({googleId:profileId});
+                
                 if(user){
                     return(done, user)
 
